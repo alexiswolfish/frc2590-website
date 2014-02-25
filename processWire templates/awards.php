6551212b3d08db983136f8d741ad41509ae12324
Andrey Efremchev <gadgefin@gmail.com>
2014-02-24 19:24:14 -0500

    auto height and weight for award page

---
+++ processWire templates/awards.php
@@ -0,0 +1,89 @@
+<?php
+/*
+	Index Page
+*/
+include("./header.inc");
+?>
+	<div id="main" class="container">
+
+		<div id="content" style="">		
+			<div id="main-container" style="margin-right: auto; margin-left: auto; width: 386px;">
+				
+				<div style="display:block; border-style:none" id="resources-container" class="border">
+					<div class="blockList" style="width:auto;height:auto">
+					    <h3 id="heading" class="border"><b>2013</b></h3>
+					    <ul><?php
+						    foreach($page->f2013awards as $link){
+							    echo "<li><a href='#' class='linkDesc'>{$link->title}<span>{$link->linkDesc}</span></a></li>";
+						    }
+					    ?></ul>
+					 </div>			    
+				    
+					<div class="blockList" style="width:auto;height:auto">
+					     <h3 id="heading" class="border"><b>2012</b></h3>
+					     <ul><?php
+						     foreach($page->f2012awards as $link){
+							     echo "<li><a href='#' class='linkDesc'>{$link->title}<span>{$link->linkDesc}</span></a></li>";
+						     }
+					     ?></ul>
+					 </div>					
+						
+					<div class="blockList" style="width:auto;height:auto">
+					     <h3 id="heading" class="border"><b>2011</b></h3>
+					     <ul><?php
+						     foreach($page->f2011awards as $link){
+							     echo "<li><a href='#' class='linkDesc'>{$link->title}<span>{$link->linkDesc}</span></a></li>";
+						     }
+					     ?></ul>
+					 </div>						
+						
+					<div class="blockList" style="width:auto;height:auto">
+					     <h3 id="heading" class="border"><b>2010</b></h3>
+					     <ul><?php
+						     foreach($page->f2010awards as $link){
+							     echo "<li><a href='#' class='linkDesc'>{$link->title}<span>{$link->linkDesc}</span></a></li>";
+						     }
+					     ?></ul>
+					 </div>				
+					
+					<div class="blockList" style="width:auto;height:auto">
+					     <h3 id="heading" class="border"><b>2009</b></h3>
+					     <ul><?php
+						     foreach($page->f2009awards as $link){
+							     echo "<li><a href='#' class='linkDesc'>{$link->title}<span>{$link->linkDesc}</span></a></li>";
+						     }
+					     ?></ul>
+					 </div>
+				     
+					<div class="blockList" style="width:auto;height:auto">
+					     <h3 id="heading" class="border"><b>2008</b></h3>
+					     <ul><?php
+						     foreach($page->f2008awards as $link){
+							     echo "<li><a href='#' class='linkDesc'>{$link->title}<span>{$link->linkDesc}</span></a></li>";
+						     }
+					     ?></ul>
+					 </div>
+				 </div>
+				    
+			 </div>
+		 </div><!--content-->
+		
+		 <aside id="sidebar">
+			
+			<!-- include sidebar from template file-->
+			<?php include("./sidebarNav.inc"); ?>
+
+			<img src="<?php echo $config->urls->templates?>images/div2.jpg">
+			<section>
+				<p>FRC Team 2590, Nemesis, is an award winning FIRST Robotics team based out of Robbinsville High School in New Jersey.
+				</p>
+				<p>Founded in 2008, the students in Nemesis routinely solve challenges in business, computer science, engineering, and math.
+				</p>
+			</section>
+			
+		</aside> <!-- sidebar -->
+		
+	</div> <!--container-->
+<?php
+include("./footer.inc");
+?>
